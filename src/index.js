648374d5e83c601625b6e3850283ddac361f1fe4
nktfmnko <nktfmnko@gmail.com>
2026-09-23 20:10:34 +0300

    рефакторинг

--- src/index.js
+++ src/index.js
@@ -4,25 +4,33 @@ function f(x) {
     return 4 * x - x ** 2
 }
 
-let x1 = parseInt(prompt("Левая граница"))
-while (isNaN(x1)) {
-    x1 = parseInt(prompt("Введите корректную левую границу"))
+function getNumberInput(promptMessage) {
+    let value = parseFloat(prompt(promptMessage))
+    while (isNaN(value)) {
+        value = parseFloat(prompt(`Некорректный ввод. ${promptMessage}`))
+    }
+    return value
 }
 
-let x2 = parseInt(prompt("Правая граница"))
-while (isNaN(x2)) {
-    x2 = parseInt(prompt("Введите корректную правую границу"))
-}
-if (x1 > x2) {
-    [x1, x2] = [x2, x1]
+function integrate(f, x1, x2, n = 10_000) {
+    if (x1 > x2) {
+        [x1, x2] = [x2, x1]
+    }
+
+    const delta = (x2 - x1) / n
+    let sum = 0
+
+    for (let i = 0; i < n; i++) {
+        const midpoint = x1 + delta * i + delta / 2
+        sum += f(midpoint) * delta
+    }
+
+    return sum
 }
 
-const n = 10_000
-const delta = (x2 - x1) / n
-let sum = 0
+let x1 = getNumberInput("Введите левую границу")
+let x2 = getNumberInput("Введите правую границу")
 
-for (let i = 0; i < n; i++) {
-    sum += f(x1 + delta / 2 + delta * i) * delta
-}
+res = integrate(f, x1, x2)
 
-alert(`res = ${sum}`)
+alert(`res = ${res.toFixed(2)}`)
